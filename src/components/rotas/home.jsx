ab3768fc3292e2313928fe6b9368ea40a1f8a353
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-02-08 11:21:51 -0300

    ADD: estrutura da home e arquivo style

--- src/components/rotas/home.jsx
+++ src/components/rotas/home.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
+
 function home() {
   return (
     <section>
         <h2>
-        Hello! I'm Lorrayne, a Front-end Developer
+        Hello!
         </h2>
+        <h2>
+        I'm Lorrayne,
+        </h2>
+        <h2>
+        a Front-end Developer
+        </h2>
+        <p>
+        Currently located in Brazil, I love design and build Practical and Simple web pages.
+        </p>
+        <button>
+          Let's get started
+        </button>
     </section>
   )
 }
