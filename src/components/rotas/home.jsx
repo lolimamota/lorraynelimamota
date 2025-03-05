ad18dea4015afc77e81891b5b4b8081fffafe410
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-03-05 20:15:47 -0300

    FIX: alinhamento do video TEST: novo video

--- src/components/rotas/home.jsx
+++ src/components/rotas/home.jsx
@@ -10,25 +10,27 @@ function home() {
   return (
     <section className={S.sectionHome}>
       <video autoPlay muted loop className={S.videoHome}>
-        <source src={BgMobile} type='video/mp4'/>
+        <source src={BgMobile} type='video/mp4' />
       </video>
-      {/* <div className={S.h2Home}>
-        <h2 className={S.h2Home}>
-        Hello!
-        </h2>
-        <h2 className={S.h2Home}>
-        I'm Lorrayne,
-        </h2>
-        <h2 className={S.h2Home}>
-        a Front-end Developer
-        </h2>
-      </div>
+      <div className={S.content}>
+        <div className={S.h2Home}>
+          <h2 className={S.h2Home}>
+            Hello!
+          </h2>
+          <h2 className={S.h2Home}>
+            I'm Lorrayne,
+          </h2>
+          <h2 className={S.h2Home}>
+            a Front-end Developer
+          </h2>
+        </div>
         <p>
-        Currently located in Brazil, I love design and build Practical and Simple web pages.
+          Currently located in Brazil, I love design and build Practical and Simple web pages.
         </p>
         <button>
           Let's get started
-        </button> */}
+        </button>
+      </div>
     </section>
   )
 }
