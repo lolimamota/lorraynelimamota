98a073b5de1c9f1ba203a49a4724d21018b07371
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2025-03-12 06:42:02 -0300

    ADD: inclusão de todas as imagens do skills and tools no projects page

---
+++ src/components/more/arrayTools.jsx
@@ -0,0 +1,10 @@
+const tools = [
+    { src: Html, alt: "Imagem que remete ao HTML", acronym: "HTML", description: "Hypertext Markup Language" },
+    { src: Css, alt: "Imagem que remete ao CSS", acronym: "CSS", description: "Cascating Style Sheet" },
+    { src: Js, alt: "Imagem que remete ao JS", acronym: "JS", description: "JavaScript program language" },
+    { src: ReactLogo, alt: "Logo do React", acronym: "REACT", description: "Biblioteca do Facebook" },
+    { src: StyledLogo, alt: "Logo do Styled-components", acronym: "STYLED-COMPONENTS", description: "Folhas de estilo integradas ao React" },
+    { src: Git, alt: "Imagem do simbolo de ramificação da worktree", acronym: "GIT AND GITHUB", description: "Ferramentas e versionamento" },
+    { src: Rote, alt: "Imagem que remete a rotas", acronym: "REACT-ROUTER-DOM", description: "Sistema de rotas" },
+    { src: Cloud, alt: "Imagem que remete a computação em nuvem", acronym: "CLOUD COMPUTING", description: "Maquinas, servidores ou serviços armazenados online" }
+  ];
--- src/components/rotas/projects.jsx
+++ src/components/rotas/projects.jsx
@@ -4,6 +4,10 @@ function projects() {
   return (
     <section>
       <h1><span>Skills</span> and Tools</h1>
+      <img src={Mapa} alt="Mapa com formato de nuvem contendo as palavras, Liderança, Pró ativa, Organizada, Metodologia Ágil, Persistência, Marketing Pessoal, Disciplina, Estudiosa, Curiosa, Facilidade em aprender, Desenvolvimento pessoal, Praticas de Comunicação não violenta, Resolução de problemas, Mentoria de carreira, Palestrante voluntária, Trabalho em equipe, Inglês básico, Interpretação de texto, Solícita e Sincera" />
+      <aside>
+        Carrousel
+      </aside>
     </section>
   )
 }
